1500887cb1564a3e66cabb8ed76fabf96be2b6a8
Chien Do <chien.dotruong@gmail.com>
2018-11-12 13:05:15 +0700

    - Finish ex1

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,9 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    temp = zeros(size(theta));
+    for i = 1:size(theta,1)
+      temp(i) = theta(i) - alpha/m * sum((X*theta - y) .* X(:,i));
+    end
+    theta = temp;
 
 
 
